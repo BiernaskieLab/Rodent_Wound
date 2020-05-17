42691903b5b4cb1b24faa4cb356d5f395f466ee1
ArzinaJaffer <ArzinaJaffer@users.noreply.github.com>
2020-05-17 13:44:16 -0600

    SCENIC_All_Cells

---
+++ SCENIC_Large_Wound_Small_Wound_Uninjuired_Fibroblasts/SCENIC.R
@@ -0,0 +1,19 @@
+setwd("/home/ssinha/SCENIC_Trial/SCENIC_WoundFibros_sample_colour_coded_1")
+library(SCENIC)
+library(GENIE3)
+library(RcisTarget)
+library(AUCell)
+scenicOptions <- readRDS("int/scenicOptions.Rds")
+
+load("x.Robj")
+exprMat <- x@raw.data
+exprMat <- as.matrix(exprMat)
+genesKept <- loadInt(scenicOptions, "genesKept")
+exprMat_filtered <- exprMat[genesKept,]
+exprMat_filtered <- log2(exprMat_filtered+1) 
+
+#runGenie3(exprMat_filtered, scenicOptions)
+#runSCENIC_1_coexNetwork2modules(scenicOptions)
+#runSCENIC_2_createRegulons(scenicOptions)
+runSCENIC_3_scoreCells(scenicOptions, exprMat)
+runSCENIC_4_aucell_binarize(scenicOptions)
