10f989b2ac3f1b47c0104552ec589ed8800dcf30
ArzinaJaffer <ArzinaJaffer@users.noreply.github.com>
2020-05-17 13:39:00 -0600

    Update SCENIC_vis.R

--- SCENIC_Large_Wound_Center_Neogenic_HF_Fibroblasts/SCENIC_vis.R
+++ SCENIC_Large_Wound_Center_Neogenic_HF_Fibroblasts/SCENIC_vis.R
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 library(SCENIC)
 scenicOptions <- readRDS("int/scenicOptions.Rds")
 scenicOptions@settings$seed <- 123
@@ -14,4 +15,22 @@ plotTsne_compareSettings(tsneCompare_high, scenicOptions, showLegend=FALSE, varN
 
 #scenicOptions@settings$defaultTsne$aucType <- "AUC"
 #scenicOptions@settings$defaultTsne$dims <- 50
+=======
+library(SCENIC)
+scenicOptions <- readRDS("int/scenicOptions.Rds")
+scenicOptions@settings$seed <- 123
+
+tsneCompare <- tsneAUC(scenicOptions, aucType="AUC", nPcs=c(5,15,50), perpl=c(5,15,50))
+tsneCompare <- tsneAUC(scenicOptions, aucType="AUC", nPcs=c(5,15,50), perpl=c(5,15,50), onlyHighConf=TRUE, filePrefix="int/tSNE_oHC")
+# Plot as pdf (individual files in int/): 
+tsneCompare <- paste0("int/",grep(".Rds", grep("tSNE_", list.files("int"), value=T), value=T))
+plotTsne_compareSettings(tsneCompare, scenicOptions, showLegend=FALSE)
+
+par(mfcol=c(3,3))
+tsneCompare_high <- paste0("int/",grep(".Rds", grep("tSNE_oHC_AUC", list.files("int"), value=T, perl = T), value=T))
+plotTsne_compareSettings(tsneCompare_high, scenicOptions, showLegend=FALSE, varName="CellType", cex=.5)
+
+#scenicOptions@settings$defaultTsne$aucType <- "AUC"
+#scenicOptions@settings$defaultTsne$dims <- 50
+>>>>>>> 30654254d0ec066466bc438c0b9bd10e7f466069
 #scenicOptions@settings$defaultTsne$perpl <- 50
